d057d428bb8ec9542f08654fc3726c390ddba0da
JustineDevs <TraderGOfficial@gmail.com>
2026-04-25 20:32:59 +0800

    refactor(frontend): adjust main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import i18n from './i18n'
 import { initTheme } from './composables/theme.js'
 import './styles/doc-layout.css'
+import './styles/app-responsive.css'
 
 initTheme()
 
