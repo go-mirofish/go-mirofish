5471e205be6b2897baaebff32213ae3c8e03807a
JustineDevs <TraderGOfficial@gmail.com>
2026-04-25 23:13:09 +0800

    refactor(frontend): adjust main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createHead } from '@unhead/vue/client'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
@@ -9,7 +10,7 @@ import './styles/app-responsive.css'
 initTheme()
 
 const app = createApp(App)
-
+app.use(createHead())
 app.use(router)
 app.use(i18n)
 
